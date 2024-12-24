f24a8e5ca418995aea9f181e16200f5aa27ab6a6
ugursaricam <ugursrcm@gmail.com>
2024-12-24 10:46:02 +0100

    modify store.html

--- store/admin.py
+++ store/admin.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_number','product_name', 'price', 'stock', 'category', 'is_available')
-    prepopulated_fields = {'slug': ('product_name',)}
+    list_display = ('product_name', 'price', 'stock', 'category', 'is_available')
     list_filter = ('category', 'is_available')
-    list_editable = ('price', 'stock', 'is_available')
-    search_fields = ('product_name', 'description')
+    search_fields = ('product_name', 'product_number')  # 'description' alanını kaldırdık
     filter_horizontal = ('allowed_users',)
 
 admin.site.register(Product, ProductAdmin)
